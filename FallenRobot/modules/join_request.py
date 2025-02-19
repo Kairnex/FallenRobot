c6f7737a11430972bbe55d729d6830ad4fbea3f3
pagal4206 <dilipjaat183@gmail.com>
2025-02-20 01:55:57 +0530

    Update join_request.py
    Signed-off-by: pagal4206 <dilipjaat183@gmail.com>

--- FallenRobot/modules/join_request.py
+++ FallenRobot/modules/join_request.py
@@ -1,21 +1,24 @@
-from telegram import Update
-from telegram.ext import CallbackContext, ChatJoinRequestHandler
+from telegram import Update, ChatMemberUpdated
+from telegram.ext import ChatMemberHandler, CallbackContext, Updater
+
 import logging
+
 
 WELCOME_MESSAGE = "Hi {name}! 👋 Welcome to the group. Feel free to participate and enjoy your time here!"
 
+
 def accept_join_request(update: Update, context: CallbackContext):
     try:
-        chat_id = update.chat_join_request.chat.id
-        user = update.chat_join_request.from_user
+        chat_id = update.effective_chat.id
+        user = update.effective_user
         first_name = user.first_name or "User"
 
-        context.bot.approve_chat_join_request(chat_id, user.id)
-        logging.info(f"Accepted join request from {first_name} in chat {chat_id}")
-
-        context.bot.send_message(chat_id=user.id, text=WELCOME_MESSAGE.format(name=first_name))
+        if update.chat_member.new_chat_member.status == "member":  
+            context.bot.send_message(chat_id=user.id, text=WELCOME_MESSAGE.format(name=first_name))
+            logging.info(f"Accepted join request from {first_name} in chat {chat_id}")
 
     except Exception as e:
         logging.error(f"Failed to accept join request: {e}")
 
-join_request_handler = ChatJoinRequestHandler(accept_join_request)
+join_request_handler = ChatMemberHandler(accept_join_request, ChatMemberHandler.CHAT_MEMBER)
+
